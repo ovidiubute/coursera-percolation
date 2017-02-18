a3e5f6d6244ac2f18ffc8d8897524626da503ca0
ovidiubute <ovidiu.bute@gmail.com>
2017-02-18 15:21:56 +0200

    Add index out of bounds checks

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,17 +1,41 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    public Percolation(int n) {}
+    private int n;
 
-    public void open(int row, int col) {}
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n > 0");
+        }
 
-    public boolean isOpen(int row, int col) {}
+        this.n = n;
+    }
 
-    public boolean isFull(int row, int col) {}
+    public void open(int row, int col) {
+        assertIndexes(row, col);
+    }
 
-    public int numberOfOpenSites() {}
+    public boolean isOpen(int row, int col) {
+        assertIndexes(row, col);
+    }
 
-    public boolean percolates() {}
+    public boolean isFull(int row, int col) {
+        assertIndexes(row, col);
+    }
+
+    public int numberOfOpenSites() {
+        return 0;
+    }
+
+    public boolean percolates() {
+        return false;
+    }
+
+    private void assertIndexes(int row, int col) {
+        if (row <= 0 || col <= 0 || row > n || col > n) {
+            throw new IndexOutOfBoundsException("1 <= row <= n & 1 <= col <= n");
+        }
+    }
 
     public static void main(String[] args) {
 
