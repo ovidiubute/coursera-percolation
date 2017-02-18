f0bedce12c218c017331ab47c44d6c7038782cd3
ovidiubute <ovidiu.bute@gmail.com>
2017-02-18 12:04:41 +0200

    Add required methods

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,8 +1,18 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    public Percolation(int n) {}
+
+    public void open(int row, int col) {}
+
+    public boolean isOpen(int row, int col) {}
+
+    public boolean isFull(int row, int col) {}
+
+    public int numberOfOpenSites() {}
+
+    public boolean percolates() {}
+
     public static void main(String[] args) {
 
     }
